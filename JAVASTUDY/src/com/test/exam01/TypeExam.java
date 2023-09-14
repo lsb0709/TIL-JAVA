bac33e99aa3948edb2d8878a6a8915af9ff14dec
lsb0709 <dltkdqor0709@gmail.com>
2023-09-14 10:06:15 +0900

    Test

---
+++ JAVASTUDY/src/com/test/exam01/TypeExam.java
@@ -0,0 +1,18 @@
+package com.test.exam01;
+
+public class TypeExam {
+    public static void main(String[] args) {
+        float num1 = 123.456755684654456353245345f; // local variables. 지역변수. 선언 + 초기화
+        double num2 = 123.34565465756745242134213f;
+
+        System.out.println("float형 변수 : " + num1);
+        System.out.println("double형 변수 : " + num2);
+
+        float f = 10.0f / 3.0f;
+        double d = 10.0 / 3.0;
+
+        System.out.println("float : " + f);
+        System.out.println("double : " + d);
+    }
+    
+}

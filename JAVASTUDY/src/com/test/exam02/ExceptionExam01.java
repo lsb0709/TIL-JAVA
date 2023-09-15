5c7792ce4eac83c65777aa8a94dacabc64647106
lsb0709 <dltkdqor0709@gmail.com>
2023-09-15 16:43:36 +0900

    0915

--- JAVASTUDY/src/com/test/exam02/ExceptionExam01.java
+++ JAVASTUDY/src/com/test/exam02/ExceptionExam01.java
@@ -1,7 +1,8 @@
 package com.test.exam02;
 
 public class ExceptionExam01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArithmeticException, Exception{
+        /* try-catch-finally 예제
         int c; // 원래 c는 양수가 나오기로 되어 있다라고 가정
         int b = 0;
         int a;
@@ -15,6 +16,34 @@ public class ExceptionExam01 {
         }finally{ // 예외가 발생하더라도 반드시 실행해야 할 명령문
             a = 100;
             System.out.println("a = " + a);
+        }
+
+        // throws 예제
+        System.out.println("a= " + a);
+         */
+
+        // int c;
+        // int b = 0;
+        // int a;
+
+        ErrorTest errorTest = new ErrorTest();
+        // errorTest.sayNickName("천재");
+        errorTest.sayNickName("fool");
+
+    }
+}
+
+class FoolException extends Exception{} // 개발자가 직접 예외처리클래스를 만들수도 있음
+
+class ErrorTest{
+    public void sayNickName(String nickname){
+
+        try{
+            if(nickname.equals("fool"))
+                throw new FoolException();
+            System.out.println("당신의 별명은 " + nickname + "입니다.");
+        }catch(FoolException e){
+            System.out.println("뭐지???");
         }
     }
 }
